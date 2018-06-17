a1a89b2a32ff02c95ef149f0c1ef257fd0a08130
sd247 <smitdesai43@gmail.com>
2018-06-17 10:07:21 -0400

    First commit

---
+++ PongClone/Assets/Resources/Scripts/PlayButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PlayButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+     
+    public void playGame(){
+        Debug.Log("Playgame was pressed!!");
+
+        // Moves to the game scene
+        SceneManager.LoadScene("Game");
+    }
+}
